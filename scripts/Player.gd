a9ba88d0fe641ebdf8e116f2f791012e92341fb9
fruits punch <fruitiestpunch@tutanota.com>
2023-05-23 09:57:51 +0200

    added animation player and animations

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var animation_player = $AnimationPlayer
+
 const ACCELERATION = 20
 const MAX_SPEED = 150
 const FRICTION = 50
@@ -14,8 +16,10 @@ func _physics_process(_delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	if(input_vector != Vector2.ZERO):
 		input_vector = input_vector.normalized()
+		animation_player.play("Run_Right_Animation")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION)
 	else:
+		animation_player.play("Idle_Right_Animation")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 
 	velocity = move_and_slide(velocity)
